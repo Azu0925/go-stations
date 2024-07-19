d0b744eee6a7f708217f8f7d861a106230be6551
Àżu <yamaneko564783@gmail.com>
2024-07-19 20:24:19 +0900

    remove: precommit

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"os"
 	"time"
 
@@ -51,6 +52,12 @@ func realMain() error {
 	mux := router.NewRouter(todoDB)
 
 	// TODO: サーバーをlistenする
+	server := http.Server{
+		Addr:    defaultPort,
+		Handler: mux,
+	}
+
+	server.ListenAndServe()
 
 	return nil
 }
